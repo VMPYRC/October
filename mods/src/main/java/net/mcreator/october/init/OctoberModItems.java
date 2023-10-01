2bb8789bf181ef79ca20ebf22e1cb6fad58a03f4
VMPYRC <90119648+VMPYRC@users.noreply.github.com>
2023-10-01 17:30:54 -0400

    multitool

--- mods/src/main/java/net/mcreator/october/init/OctoberModItems.java
+++ mods/src/main/java/net/mcreator/october/init/OctoberModItems.java
@@ -8,16 +8,19 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.Registry;
 
+import net.mcreator.october.item.MultiToolItem;
 import net.mcreator.october.item.AceItem;
 import net.mcreator.october.OctoberMod;
 
 public class OctoberModItems {
+	public static Item MULTI_TOOL;
 	public static Item ACE_HELMET;
 	public static Item ACE_CHESTPLATE;
 	public static Item ACE_LEGGINGS;
 	public static Item ACE_BOOTS;
 
 	public static void load() {
+		MULTI_TOOL = Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(OctoberMod.MODID, "multi_tool"), new MultiToolItem());
 		ACE_HELMET = Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(OctoberMod.MODID, "ace_helmet"), new AceItem.Helmet());
 		ACE_CHESTPLATE = Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(OctoberMod.MODID, "ace_chestplate"), new AceItem.Chestplate());
 		ACE_LEGGINGS = Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(OctoberMod.MODID, "ace_leggings"), new AceItem.Leggings());
---
+++ mods/src/main/java/net/mcreator/october/item/MultiToolItem.java
@@ -0,0 +1,97 @@
+
+package net.mcreator.october.item;
+
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.TieredItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.core.BlockPos;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+
+import com.google.common.collect.Multimap;
+import com.google.common.collect.ImmutableMultimap;
+
+public class MultiToolItem extends TieredItem {
+	public MultiToolItem() {
+		super(new Tier() {
+			public int getUses() {
+				return 10000;
+			}
+
+			public float getSpeed() {
+				return 50f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 998f;
+			}
+
+			public int getLevel() {
+				return 5;
+			}
+
+			public int getEnchantmentValue() {
+				return 50;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of();
+			}
+		}, new Item.Properties().fireResistant());
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(content -> content.accept(this));
+	}
+
+	@Override
+	public boolean isCorrectToolForDrops(BlockState blockstate) {
+		int tier = 5;
+		if (tier < 3 && blockstate.is(BlockTags.NEEDS_DIAMOND_TOOL)) {
+			return false;
+		} else if (tier < 2 && blockstate.is(BlockTags.NEEDS_IRON_TOOL)) {
+			return false;
+		} else {
+			return tier < 1 && blockstate.is(BlockTags.NEEDS_STONE_TOOL)
+					? false
+					: (blockstate.is(BlockTags.MINEABLE_WITH_AXE) || blockstate.is(BlockTags.MINEABLE_WITH_HOE) || blockstate.is(BlockTags.MINEABLE_WITH_PICKAXE) || blockstate.is(BlockTags.MINEABLE_WITH_SHOVEL));
+		}
+	}
+
+	@Override
+	public float getDestroySpeed(ItemStack itemstack, BlockState blockstate) {
+		return 50f;
+	}
+
+	@Override
+	public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot equipmentSlot) {
+		if (equipmentSlot == EquipmentSlot.MAINHAND) {
+			ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+			builder.putAll(super.getDefaultAttributeModifiers(equipmentSlot));
+			builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Tool modifier", 998f, AttributeModifier.Operation.ADDITION));
+			builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Tool modifier", 46, AttributeModifier.Operation.ADDITION));
+			return builder.build();
+		}
+		return super.getDefaultAttributeModifiers(equipmentSlot);
+	}
+
+	@Override
+	public boolean hurtEnemy(ItemStack stack, LivingEntity entity, LivingEntity sourceentity) {
+		stack.hurtAndBreak(2, sourceentity, i -> i.broadcastBreakEvent(EquipmentSlot.MAINHAND));
+		return true;
+	}
+
+	@Override
+	public boolean mineBlock(ItemStack stack, Level world, BlockState state, BlockPos pos, LivingEntity entity) {
+		stack.hurtAndBreak(1, entity, i -> i.broadcastBreakEvent(EquipmentSlot.MAINHAND));
+		return true;
+	}
+}
